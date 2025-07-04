2693e4a9ae84e635c4c93fe02a5e14748576071e
Konstantin Voronin <heavensmiting@gmail.com>
2025-07-04 17:17:44 +0300

    реализован модуль для запуска программы

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.ui import user_interface_with_keyword
+from src.utils import setup_and_fill_database
+
+
+if __name__ == "__main__":
+    """ Запуск программы"""
+
+    db_manager = setup_and_fill_database("hh_db")
+    user_interface_with_keyword(db_manager)
